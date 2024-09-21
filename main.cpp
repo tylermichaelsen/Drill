e23d2720cd1d424ccee02221f71b6e248d987fbc
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-21 03:48:33 -0500

    Change comment

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
-// get continuous input
+// Chapter 4 Drill
 
 #include "PPP.h"
 
 
 int main() {
-	cout << "Enter an equation: ";
-	double num1, num2, result;
-	char operation;
-	while (cin >> num1 >> operation >> num2) {
-		switch (operation) {
-			case '+':
-				result = num1 + num2;
-				break;
-			case '-':
-				result = num1 - num2;
-				break;
-			case '*':
-				result = num1 * num2;
-				break;
-			case '/':
-				result = num1 / num2;
-				break;
-			default:
-				cout << "Dont know that operator.\n";
-		}
-		cout << num1 << " " << operation << " " << num2 << " = " << result << endl;
-	}
+	
 }
