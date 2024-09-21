082c79f44d64b3c778c6968bcd791141b3cc4be8
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-21 03:50:26 -0500

    Write drill 4 beginning

--- main.cpp
+++ main.cpp
@@ -4,5 +4,17 @@
 
 
 int main() {
-	
+	try {
+		return 0;
+	}
+
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		return 1;
+	}
+
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		return 2;
+	}
 }
